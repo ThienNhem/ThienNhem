49359f0279691eec80d4421a16e72db66db62904
ThienNhem <122137580+ThienNhem@users.noreply.github.com>
2023-01-07 12:01:36 +0700

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+ int main()
+{
+      int  x,y; /*places to store numbers */places to store numbers */
+      printf("Enter x ");
+      scanf("%d", &x);
+      printf("Enter y ");
+      scanf("%d",&y);
+      printf("The sum of x and y was %d\n",x+y);
+      }
+
+      
